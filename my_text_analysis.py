c4a1f70771aff6fa3c96558d80f112ca2288ed35
Eleonore9 <eleonore.mayola@ymail.com>
2014-05-14 00:49:35 +0200

    the bar chart s not working yet but added a function for it.

--- my_text_analysis.py
+++ my_text_analysis.py
@@ -1,4 +1,5 @@
-import count_words_letters as cwl
+import count_words_letters_class as cwl
+import matplotlib.pyplot as plt
 
 CWL = cwl.count_words_letters()
 
@@ -13,8 +14,14 @@ def list_for_plot_letters(input_txt_file):
         dict_letters['letters'].append(k)
     return dict_letters
 
+def plot_word_letters(dict_plot):
+    fig = plt.figure()
+    ax = plt.subplot(111)
+    ax.bar(dict_plot['letters'], dict_plot['count'], width=100)
+    return plt.show()
 
 if __name__ == "__main__":
 #count words and letters for two sample texts:
-    print list_for_plot_letters('sample.txt')
+    data_plt = list_for_plot_letters('sample.txt')
+    plot_word_letters(data_plt)
     #print test.words_freq('sample.txt')
