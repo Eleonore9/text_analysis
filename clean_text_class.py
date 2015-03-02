64d63cf69fcb94aa39ac6ffa40b41a9daba0c1de
Eleonore9 <eleonore.mayola@ymail.com>
2015-03-02 00:34:24 +0000

    WIP: preparing text for analysis.
    Can retrieve either list of words
    or letters for amalysis.

--- clean_text_class.py
+++ clean_text_class.py
@@ -15,7 +15,7 @@ class clean_text:
         ''' Build a dictionary with metadata from
         the top of the text file'''
         text_file = open(text, 'r')
-        meta =  text_file.read(265)
+        meta =  text_file.read(590)
         title, author, language = "", "", ""
         try:
             t = re.search("Title: [A-Za-z'\t' .]+", meta)
@@ -25,7 +25,7 @@ class clean_text:
             author += a.group(0)
             language += l.group(0)
         except:
-            pass
+            print "No metadata found!"
         metadata = {"title": title.replace("Title: ", ""),
                     "author": author.replace("Author: ", ""),
                     "language": language.replace("Language: ", "")}
@@ -35,32 +35,45 @@ class clean_text:
         ## Helpers to actually clean the whole text:
         ##First remove the the metadata
     def remove_metadata(self, text):
+        ''' Returns the text as a string
+        without the metadata at the top.'''
         text_file = open(text, 'r')
-        text_file.read(265)
+        text_file.read(683)
         clean_text = text_file.read().strip().lower()
-        #print clean_text
         text_file.close()
         return clean_text
         
+    def remove_chapters(self, text_str):
+        ''' Returns the whole text as a string
+        without "Chapter x".'''
+        chapters = re.compile("chapter \w+.")
+        clean_text = chapters.sub("", text_str)
+        return clean_text
+
     def remove_punctuation(self, text_str):
         """Returns a string without 
         punctuation"""
-        punc = re.compile("[,\.\(\)-_\*:]")
+        punc = re.compile("[,\.\(\)_\*:?!']")
         clean_text = punc.sub("", text_str)
         return clean_text
 
     # Add a function to return a list of words
-    def text_words(self, text_cleaned):
+    def whole_text_words(self, text_cleaned):
         list_words = text_cleaned.split()
         return list_words
     
     # Add a function to return a string of letters
     def text_letters(self, text_cleaned):
-        pass
+        list_letters = [l for l in text_cleaned if l not in ['\n', ' ']]
+        return list_letters
+
 
 if __name__ == "__main__":
-    test = clean_text('books/test.txt')
+    test = clean_text('books/test2.txt')
+    #print test.store_metadata(test.text)
     no_meta = test.remove_metadata(test.text)
-    print no_meta
-    cleaned = test.remove_punctuation(no_meta)
-    print test.text_words(cleaned)
+    #print no_meta
+    print test.remove_chapters(no_meta)
+    #cleaned = test.remove_punctuation(no_meta)
+    #print test.whole_text_words(cleaned)
+    #print test.text_letters(cleaned)
