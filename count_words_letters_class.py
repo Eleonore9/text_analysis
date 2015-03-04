f8bb3d14a30e44c3c15139ac045b73a147dbe405
Eleonore9 <eleonore.mayola@ymail.com>
2015-03-04 22:09:48 +0000

    Refactored the count class a bit.
    Words and letters "analysis" are now
    two distinct classes and the letters
    freq are output as a list of tuples.

--- count_words_letters_class.py
+++ count_words_letters_class.py
@@ -2,28 +2,16 @@ import string
 import matplotlib.pyplot as plt
 import clean_text_class as ct
 
-class count_words_letters:
-    '''Analyse the number of words and letters 
+class analyse_words:
+    '''Analyse the words 
     in a text file '''
     
-    
-    def __init__(self, words, letters):
+    def __init__(self, words):
         self.words = words
-        self.letters = letters
-        self.alphabet = [l for l in string.ascii_lowercase]
 
-    # Methods to count words/letters in a text file
-    # Returns a dict with the letter/word as key and
-    # the count as value. Ex: {'a': 7, 'f': 3}
-    def letters_count(self, letters):
-        """
-        Returns a dictionary with all
-        the letters present in the text
-        and their frequency
-        """
-        l = sorted(letters)
-        letters_dict = {a:l.count(a) for a in self.alphabet}
-        return letters_dict
+    # Methods to count words in a text file
+    # Returns a dict with the word as key and
+    # the count as value. 
 
     def words_count(self, words):
         """
@@ -35,22 +23,46 @@ class count_words_letters:
         words_dict = {w:l.count(w) for w in l}
         return words_dict
 
+
+class analyse_letters:
+    '''Analyse the number of letters 
+    in a text file '''
+    
+    
+    def __init__(self, letters):
+        self.letters = letters
+        self.alphabet = [l for l in string.ascii_lowercase]
+
+    # Methods to count letters in a text file
+    # Returns a dict with the letter as key and
+    # the count as value. 
+    def letters_count(self, letters):
+        """
+        Returns a dictionary with all
+        the letters present in the text
+        and their frequency
+        """
+        l = sorted(letters)
+        letters_dict = {a:l.count(a) for a in self.alphabet}
+        return letters_dict
+
     # Methods to retrieve letters frequencies and plot them.
     def letters_freq(self, letters_dict):
         """
-        Returns a dictionary with all
+        Returns a list of tuples with all
         the letters and their percentage
         """
         total = sum(letters_dict.values())
-        freq_letters = {k:round(v *100./total, 1) for k, v in letters_dict.items()}
-        return freq_letters
+        freq_dict = {k:round(v *100./total, 1) for k, v in letters_dict.items()}
+        freq_list = sorted(freq_dict.iteritems()) #Keeps the letters ordered
+        return freq_list
     
-    def plot_letters(self, dict_freq):
+    def plot_letters(self, d):
         """
         Trying to plot the frequencies
         with matplotlib.
         """
-        d = sorted(dict_freq.iteritems())
+        #d = sorted(dict_freq.iteritems())
         letters, freqs = tuple(x[0] for x in d), tuple(x[1] for x in d)
         print list(freqs)
         plt.title("Letters content (in %)")
@@ -61,12 +73,15 @@ class count_words_letters:
         plt.grid(True)
         return plt.show()
 
+
 if __name__ == "__main__":
     test = ct.clean_text('books/test2.txt')
     my_text = test.remove_punctuation(test.remove_chapters(test.remove_metadata(test.text)))
     my_words = test.whole_text_words(my_text)
     my_letters = test.text_letters(my_text)
-    cwl = count_words_letters(my_words, my_letters)
-    l_freq = cwl.letters_freq(cwl.letters_count(cwl.letters))
+    cw = analyse_words(my_words)
+    print cw.words_count(cw.words)
+    cl = analyse_letters(my_letters)
+    l_freq = cl.letters_freq(cl.letters_count(cl.letters))
     print l_freq
-    cwl.plot_letters(l_freq)
+    #cwl.plot_letters(l_freq)
