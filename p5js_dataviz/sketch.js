485e7eb0bbe54df9cb739f1c998a8a2339dcc87c
Eleonore9 <eleonore.mayola@ymail.com>
2019-07-03 16:45:14 +0200

    Analysis visualisation using p5.js [WIP]

--- p5js_dataviz/sketch.js
+++ p5js_dataviz/sketch.js
@@ -1,32 +1,24 @@
 let table;
 
 function preload() {
-  table = loadTable('letters_freq_en_vs_fr.csv', 'csv', 'header');
+  table = loadTable('letters_freq_en_fr_colours.csv', 'csv', 'header');
 }
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(1200, 600);
   noStroke();
 }
 
-let data = [{letter: "a", freq: 8},
-            {letter: "b", freq: 1},
-            {letter: "c", freq: 3}];
-let colours = ["#47aef7", "#43cc8c", "#f2ec7d", "#cf84f4", "#f280d0", "#", "#",
-              "#", "#", "#", "#", "#", "#",
-              "#", "#","#", "#","#", "#", "#",
-              "#", "#","#", "#","#", "#"];
-
 function draw() {
   let x_coord = 0;
-  background("#424040");
+  background("#808080");
   for (i=0; i<table.getRowCount(); i++){
-    letter = table.getColumn('letter');
-    freq = table.getColumn('English');
+    letter = table.get(i, 'letter');
+    freq = table.get(i, 'English');
     side = freq * 10;
-    fill(colours[i]);
+    fill(table.get(i, 'colour'));
     text(letter, 30 + x_coord, 20);
     square(30 + x_coord, 30, side, 2);
-    x_coord = x_coord + side + 3;
+    x_coord = x_coord + side + 5;
   }
 }
